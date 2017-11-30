ca5727fc04472b078cdc8450683ba47356f73530
Victor Alves <alvesvictorit@gmail.com>
2017-11-30 21:45:04 -0200

    Novo dataPicker e construtor

--- app/src/main/java/com/eventos/fragment/AlterarPerfilFragment.java
+++ app/src/main/java/com/eventos/fragment/AlterarPerfilFragment.java
@@ -35,14 +35,18 @@ import com.eventos.app.AppConfig;
 import com.eventos.app.AppController;
 import com.eventos.bean.UsuarioBean;
 import com.eventos.helper.DatePickerDataNascimento;
+import com.eventos.helper.DatePickerDataNascimentoAlterar;
 import com.eventos.helper.SessionManager;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.UnsupportedEncodingException;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class AlterarPerfilFragment extends android.app.Fragment {
@@ -54,7 +58,7 @@ public class AlterarPerfilFragment extends android.app.Fragment {
     private SessionManager sessionManager;
     private String emailLogado, senhaLogado;
     private ProgressDialog progressDialog;
-    private DatePickerDataNascimento datePickerDialog;
+    private DatePickerDataNascimentoAlterar datePickerDialog;
 
 
     public AlterarPerfilFragment() {
@@ -76,7 +80,7 @@ public class AlterarPerfilFragment extends android.app.Fragment {
 
         nome = (EditText) view.findViewById(R.id.nome_usuario_alterar);
         telefone = (EditText) view.findViewById(R.id.telefone_usuario_alterar);
-        btn_alterar = (Button) view.findViewById(R.id.btn_alterarSenha);
+        btn_alterar = (Button) view.findViewById(R.id.btn_alterar);
         btn_datNascimento = (Button) view.findViewById(R.id.data_nascimento_usuario_alterar);
         btn_desativar = (Button) view.findViewById(R.id.btn_link_desativar);
         spinnerSexo = (Spinner) view.findViewById(R.id.spinner_sexo_alterar);
@@ -88,7 +92,15 @@ public class AlterarPerfilFragment extends android.app.Fragment {
 
         //METODO UTILIZADO PARA PREENCHER OS CAMPOS DO USUÁRIO
         preencheCampos(usuarioBean);
+        //Botao para desativar usuário
 
+        btn_datNascimento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datePickerDialog = new DatePickerDataNascimentoAlterar();
+                datePickerDialog.show(getFragmentManager(),"Data de Nascimento");
+            }
+        });
         btn_desativar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -96,11 +108,136 @@ public class AlterarPerfilFragment extends android.app.Fragment {
             }
         });
 
+        //Botão para alterar os dados do usuário
+        btn_alterar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int posicaoSpinner = spinnerSexo.getSelectedItemPosition();
+                char sexoUsuario = ' ';
+                String dataNascimentoUsuario = btn_datNascimento.getText().toString();
+                String nomeUsuario = nome.getText().toString().trim();
+                String telefoneUsuario = telefone.getText().toString().trim();
+                switch (posicaoSpinner){
+                    case 1: sexoUsuario = 'M';
+                        break;
+                    case 2: sexoUsuario = 'F';
+                        break;
+                    case 3: sexoUsuario = 'O';
+                        break;
+                    case 4: sexoUsuario = 'N';
+                        break;
+                }
+                Toast toastMensagemErro;
+
+                if(!validaNome(nomeUsuario)){
+                    toastMensagemErro = Toast.makeText(getActivity().getBaseContext(),"NOME INCORRETO",Toast.LENGTH_SHORT);
+                    toastMensagemErro.show();
+                }
+                else if(telefoneUsuario.isEmpty() || telefoneUsuario.equals(" ")){
+                    toastMensagemErro = Toast.makeText(getActivity().getBaseContext(),"CAMPO TELEFONE VAZIO",Toast.LENGTH_SHORT);
+                    toastMensagemErro.show();
+                }
+                else if(dataNascimentoUsuario.isEmpty() || dataNascimentoUsuario.equals(" ") || !validaIdadeUsuario(dataNascimentoUsuario)){
+                    toastMensagemErro = Toast.makeText(getActivity().getBaseContext(),"É NECESSÁRIO TER 18 ANOS PARA USAR O APP",Toast.LENGTH_SHORT);
+                    toastMensagemErro.show();
+                }
+                else if(sexoUsuario == ' '){
+                    toastMensagemErro = Toast.makeText(getActivity().getBaseContext(),"CAMPO SEXO VAZIO",Toast.LENGTH_SHORT);
+                    toastMensagemErro.show();
+                }
+                else if(nomeUsuario.length() < 3){
+                    toastMensagemErro = Toast.makeText(getActivity().getBaseContext(),"NOME INVÁLIDO",Toast.LENGTH_SHORT);
+                    toastMensagemErro.show();
+                }
+                else{
+                    try {
+                        usuarioBean = new UsuarioBean(nomeUsuario, emailLogado, senhaLogado, dataPadraoSQL(dataNascimentoUsuario), sexoUsuario, telefoneUsuario);
+                    }
+                    catch(Exception e){
+                        Log.e("Exception",e.getMessage());
+                    }
+                    alteraUsuario(usuarioBean);
+                }
+            }
+        });
         setupToolbar(view);
         return view;
     }
 
-    //ARRUMAR ESSA PARTE DE CARREGAR OS DADOS NA TELA.
+    public void alteraUsuario(final UsuarioBean usuarioBean){
+
+    }
+
+
+    public boolean validaNome(String nome){
+        Pattern pattern = Pattern.compile("^[[ ]|\\p{L}*]+$");
+        Matcher matcher = pattern.matcher(nome);
+        return matcher.matches();
+    }
+
+    public boolean validaIdadeUsuario(String dataNascimentoUsuario){
+        int posPrimDaBarra = dataNascimentoUsuario.indexOf("/");
+        int posSegDaBarra = dataNascimentoUsuario.lastIndexOf("/");
+        int dataNascimentoUsuarioEmHoras = 0;
+        int ano,mes,dia;
+        if(posPrimDaBarra == 1){
+            if(posSegDaBarra == 3){
+                ano = Integer.parseInt(dataNascimentoUsuario.substring(4,8));
+                mes = Character.getNumericValue(dataNascimentoUsuario.charAt(2));
+                dia = Character.getNumericValue(dataNascimentoUsuario.charAt(0));
+            }
+            else{
+                ano = Integer.parseInt(dataNascimentoUsuario.substring(5,9));
+                mes = Integer.parseInt(dataNascimentoUsuario.substring(2,4));
+                dia = Character.getNumericValue(dataNascimentoUsuario.charAt(0));
+            }
+        }
+        else{
+            if(posSegDaBarra == 4){
+                ano = Integer.parseInt(dataNascimentoUsuario.substring(5,9)) ;
+                mes = Character.getNumericValue(dataNascimentoUsuario.charAt(3));
+                dia = Integer.parseInt(dataNascimentoUsuario.substring(0,2));
+            }
+            else{
+                ano = Integer.parseInt(dataNascimentoUsuario.substring(6,10));
+                mes = Integer.parseInt(dataNascimentoUsuario.substring(3,5));
+                dia = Integer.parseInt(dataNascimentoUsuario.substring(0,2));
+            }
+        }
+        dataNascimentoUsuarioEmHoras = ano * 8760 + mes * 730 + dia * 24;
+        Calendar hoje = Calendar.getInstance();
+        int dataAtualEmHoras = hoje.get(Calendar.YEAR) * 8760 + (hoje.get(Calendar.MONTH)+1) * 730 + hoje.get(Calendar.DAY_OF_MONTH) * 24;
+        int diferencaEntreDatasHoras = dataAtualEmHoras - dataNascimentoUsuarioEmHoras;
+        int idadeMinimaEmHoras = 18*8760;
+        if(diferencaEntreDatasHoras >= idadeMinimaEmHoras){
+            return true;
+        }
+        return false;
+    }
+
+    public String dataPadraoSQL(String dataNascimentoUsuario){
+        int posPrimDaBarra = dataNascimentoUsuario.indexOf("/");
+        int posSegDaBarra = dataNascimentoUsuario.lastIndexOf("/");
+        String data = null;
+        if(posPrimDaBarra == 1){
+            if(posSegDaBarra == 3){
+                data = dataNascimentoUsuario.substring(4,8)+"-"+dataNascimentoUsuario.charAt(2)+"-"+dataNascimentoUsuario.charAt(0);
+            }
+            else{
+                data = dataNascimentoUsuario.substring(5,9) +"-"+ dataNascimentoUsuario.substring(2,4) +"-"+ dataNascimentoUsuario.charAt(0);
+            }
+        }
+        else{
+            if(posSegDaBarra == 4){
+                data = dataNascimentoUsuario.substring(5,9) +"-"+ dataNascimentoUsuario.charAt(3) +"-"+ dataNascimentoUsuario.substring(0,2);
+            }
+            else{
+                data = dataNascimentoUsuario.substring(6,10) +"-"+ dataNascimentoUsuario.substring(3,5) +"-"+ dataNascimentoUsuario.substring(0,2);
+            }
+        }
+        return data;
+    }
+
     public void preencheCampos(final UsuarioBean usuarioBean){
         //String utilizada para cancelar a requisição
         String tag_req = "req_registro";
@@ -266,7 +403,7 @@ public class AlterarPerfilFragment extends android.app.Fragment {
     public String dataPadraoBR(String dataNascimentoUsuario){
         int tamanho = dataNascimentoUsuario.length() - 1;
         int posSegDaBarra = dataNascimentoUsuario.lastIndexOf("-");
-        String data = null;
+        String data;
         if(posSegDaBarra == 7) {
             if (tamanho == 9) {
                 data = dataNascimentoUsuario.substring(8) + "/" + dataNascimentoUsuario.substring(5, 7) + "/" + dataNascimentoUsuario.substring(0, 4);
